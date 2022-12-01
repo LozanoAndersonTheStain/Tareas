be7ef9e46629ad767cedd379226754f5aceb469f
LozanoTheStain <adslozanov@gmail.com>
2022-12-01 07:50:08 -0500

    Realizando sistema de muestra de tareas

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Tasks.css'
 
 const Tasks = () => {
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])
+  const [editMode, setEditMode] = useState(false)
+
+  useEffect(() => {
+  }, [])
+
   return (
     <div className="container mt-5">
       <h1 className="text-primary text-center">Tareas</h1>
+      <hr />
+      <div className="row">
+        <div className="col-md-8 col-sm-12">
+          <h4 className="text-center">Lista de Tareas</h4>
+        </div>
+        <div className="col-md-4 col-sm-12">
+          <h4>{editMode ? 'Modificar Tarea' : 'Agregar Tarea'}</h4>
+
+          <form onSubmit={editMode ? saveTask : addTask}>
+            <textarea
+              type="input"
+              placeholder="Ingrese tarea..."
+              className="form-control input-task"
+              onChange={(e) => setTask(e.target.value)}
+              value={task}
+            />
+
+            <button
+              type="submit"
+              className={
+                editMode
+                  ? 'btn btn-warning w-100 mt-2'
+                  : 'btn btn-dark w-100 mt-2'
+              }
+            >
+              {editMode ? 'Guardar Tarea' : 'Agregar Tarea'}
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/helpers/Actions.js
@@ -0,0 +1,35 @@
+import { initializeApp } from 'firebase/app'
+import { addDoc, collection, getFirestore } from 'firebase/firestore/lite'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAVBn4BHxrwOw5hW1j1em4Q5BrL68FH3_A',
+  authDomain: 'tareas-c13-alv.firebaseapp.com',
+  projectId: 'tareas-c13-alv',
+  storageBucket: 'tareas-c13-alv.appspot.com',
+  messagingSenderId: '214391096302',
+  appId: '1:214391096302:web:72ffa5d4be9ac4efc584ff',
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export const getCollection = async (coll) => {
+  const result = {
+    statusResponse: false,
+    data: null,
+    erroe: null,
+  }
+  try {
+    const res = collection(db, coll)
+    const data = await addDoc(res)
+    const arrayData = data.docs.map((element) => ({
+      id: element.id,
+      ...element.data(),
+    }))
+    result.statusResponse = true
+    result.data = arrayData
+  } catch (error) {
+    result.error = error
+  }
+  return result
+}
